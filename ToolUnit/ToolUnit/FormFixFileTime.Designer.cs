12926b6484815b0f042f3b9bddb9517ae892e418
wb_hyl <982857141.com>
2019-08-09 15:32:22 +0800

    add

---
+++ ToolUnit/ToolUnit/FormFixFileTime.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ToolUnit
+{
+    partial class FormFixFileTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_filePath = new System.Windows.Forms.TextBox();
+            this.button_select = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // datePicker
+            // 
+            this.datePicker.CustomFormat = "";
+            this.datePicker.Location = new System.Drawing.Point(123, 9);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(142, 21);
+            this.datePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请选择日期和时间";
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Location = new System.Drawing.Point(274, 9);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(142, 21);
+            this.textBox_time.TabIndex = 2;
+            this.textBox_time.Text = "12:00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "请选择路径";
+            // 
+            // textBox_filePath
+            // 
+            this.textBox_filePath.Location = new System.Drawing.Point(123, 41);
+            this.textBox_filePath.Name = "textBox_filePath";
+            this.textBox_filePath.Size = new System.Drawing.Size(392, 21);
+            this.textBox_filePath.TabIndex = 4;
+            this.textBox_filePath.TextChanged += new System.EventHandler(this.textBoxOnTextChange);
+            // 
+            // button_select
+            // 
+            this.button_select.Location = new System.Drawing.Point(526, 39);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(44, 23);
+            this.button_select.TabIndex = 5;
+            this.button_select.Text = "...";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(1, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(679, 306);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "名称";
+            this.columnHeader1.Width = 390;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "修改时间";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "文件类型";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 135;
+            // 
+            // FormFixFileTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 404);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button_select);
+            this.Controls.Add(this.textBox_filePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_time);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.datePicker);
+            this.Name = "FormFixFileTime";
+            this.Text = "修改文件时间";
+            this.Load += new System.EventHandler(this.FormFixFileTime_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_time;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_filePath;
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
---
+++ ToolUnit/ToolUnit/FormFixFileTime.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolUnit
+{
+    public partial class FormFixFileTime : Form
+    {
+        private string m_QueryPath;
+        public FormFixFileTime()
+        {
+            InitializeComponent();
+        }
+
+        private void FormFixFileTime_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                textBox_filePath.Text = foldPath;
+            }
+        }
+
+        private void textBoxOnTextChange(object sender, EventArgs e)
+        {
+            m_QueryPath = textBox_filePath.Text.Trim();
+            if (!Directory.Exists(m_QueryPath)) return;
+            //MessageBox.Show(m_QueryPath);
+            DirectoryInfo di = new DirectoryInfo(m_QueryPath);
+            listView1.Items.Clear();
+
+            foreach(FileSystemInfo fsi in di.GetFileSystemInfos())
+            {
+                ListViewItem item = listView1.Items.Add(fsi.Name);
+                item.SubItems.Add(fsi.LastWriteTime.ToString());
+
+                if (fsi is DirectoryInfo)
+                {
+                    item.ForeColor = Color.Blue;
+                    item.SubItems.Add("文件夹");
+                }
+                else
+                {
+                    item.SubItems.Add("文件");
+                }
+            }
+        }
+    }
+}
