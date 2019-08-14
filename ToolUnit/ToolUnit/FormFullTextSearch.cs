d3df9dcb0a20b60ef98713f43dedf065582c5c23
wb_hyl <982857141.com>
2019-08-14 17:53:17 +0800

    add

--- ToolUnit/ToolUnit/FormFullTextSearch.cs
+++ ToolUnit/ToolUnit/FormFullTextSearch.cs
@@ -19,7 +19,18 @@ namespace ToolUnit
 
         private void button_selectFile_Click(object sender, EventArgs e)
         {
-
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (textBox_filePath.Text.Length > 0)
+            {
+                dialog.SelectedPath = textBox_filePath.Text;
+            }
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                if (foldPath[foldPath.Length - 1] != '\\') foldPath += "\\";
+                textBox_filePath.Text = foldPath;
+            }
         }
     }
 }
